55fca6aa944005a10fcf6d8488d66c82d739c6b8
hajteyib <hadjteyib2@gmail.com>
2025-09-24 14:38:01 +0000

    Fix imports in training package

--- src/training/__init__.py
+++ src/training/__init__.py
@@ -1,16 +1,2 @@
 # src/training/__init__.py
-"""
-Training package for HTR contrastive learning.
-Contains trainer and monitoring classes.
-"""
-
-
-from .trainer import OptimizedContrastiveTrainer
-from .monitor import ContrastiveMonitor, ContrastiveMetrics
-
-__all__ = [
-    'OptimizedContrastiveTrainer',
-    'ContrastiveTrainer',  # Alias for compatibility
-    'ContrastiveMonitor',
-    'ContrastiveMetrics'
-]
+from .trainer_advanced import AdvancedTrainer
